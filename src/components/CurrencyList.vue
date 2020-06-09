1da066f3e4b562587c279ec86dd6696fb878901e
Igor Trofimov <igortr333@gmail.com>
2020-06-10 00:05:36 +0400

    turn to coffee

--- src/components/CurrencyList.vue
+++ src/components/CurrencyList.vue
@@ -39,65 +39,56 @@
   </section>
 </template>
 
-<script>
+<script lang="coffee">
   import { getRates } from '../lib/api'
   import { Parser } from 'expr-eval'
 
-  const fixed = (value) => +value.toFixed(2)
+  fixed = (value) -> +value.toFixed 2
 
-  export default {
-    name: 'CurrencyList',
-    data() {
-      return {
-        rates: null,
-        selectedCurrencies: ['RUB', 'USD', 'EUR'],
-        base: 'USD',
-        amount: 100,
-        rawAmount: '100',
-      }
-    },
-    computed: {
-      currencies() {
-        return this.selectedCurrencies.map((currency) => ({ code: currency, rate: this.rates[currency] }))
-      },
-      availableCurrencies() {
-        return Object.keys(this.rates).sort()
-      },
-      currentRate() {
-        return this.rates[this.base]
-      },
-    },
-    methods: {
-      handleChange(e) {
-        const parser = new Parser()
-        this.rawAmount = e.target.value
-        try {
-          this.amount = parser.evaluate(this.rawAmount) / this.currentRate
-        } catch (e) {
-          //  ignoring wrong math expression
-        }
-      },
-      resolveAmount(currencyCode) {
-        return currencyCode === this.base
-          ? this.rawAmount
-          : fixed(this.amount * this.rates[currencyCode])
-      },
-      setBaseCurrency(currencyCode) {
-        this.base = currencyCode
-        this.rawAmount = fixed(this.amount * this.currentRate)
-      },
-      toggleCurrency(e) {
-        if (e.target.checked) this.selectedCurrencies.push(e.target.value)
-        else this.removeCurrency(e.target.value)
-      },
-      removeCurrency(currencyCode) {
-        this.selectedCurrencies = this.selectedCurrencies.filter(code => code !== currencyCode)
-      },
-    },
-    mounted() {
-      getRates().then((response) => this.rates = response.rates)
-    },
-  }
+  export default
+    name: 'CurrencyList'
+    data: ->
+      rates: null
+      selectedCurrencies: ['RUB', 'USD', 'EUR']
+      base: 'USD'
+      amount: 100
+      rawAmount: '100'
+
+    computed:
+      currencies: ->
+        { code: currency, rate: @rates[currency] } for currency in @selectedCurrencies
+      availableCurrencies: -> Object.keys(@rates).sort()
+      currentRate: -> @rates[@base]
+
+    methods:
+      handleChange: (e) ->
+        parser = new Parser()
+        @rawAmount = e.target.value
+        try
+          @amount = parser.evaluate(@rawAmount) / @currentRate
+      resolveAmount: (currencyCode) ->
+        if currencyCode is @base
+          @rawAmount
+        else
+          fixed @amount * @rates[currencyCode]
+
+      setBaseCurrency: (currencyCode) ->
+        @base = currencyCode
+        @rawAmount = fixed @amount * @currentRate
+
+      toggleCurrency: (e) ->
+        { checked, value } = e.target
+        if checked
+          @selectedCurrencies.push(value)
+        else
+          @removeCurrency(value)
+
+      removeCurrency: (currencyCode) ->
+        @selectedCurrencies = (code for code in @selectedCurrencies when code isnt currencyCode)
+
+    mounted: ->
+      response = await getRates()
+      @rates = response.rates
 </script>
 
 <style scoped>
