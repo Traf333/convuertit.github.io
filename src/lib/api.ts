6e6798d51857e697a51e2980dcbbd71ba61dbad3
Igor Trofimov <igortr333@gmail.com>
2021-04-11 19:48:52 +0200

    add token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import { RateResponse } from "../../types";
 
 const getRates = (base = "USD"): Promise<RateResponse> =>
-  fetch(`https://api.exchangeratesapi.io/latest?base=${base}&access_key=63130c37bb3b50ae99edfcca6f8c43bd`).then(res =>
+  fetch(`http://api.exchangeratesapi.io/latest?base=${base}&access_key=63130c37bb3b50ae99edfcca6f8c43bd`).then(res =>
     res.json()
   );
 
